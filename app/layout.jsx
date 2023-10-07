404967414b47e8c73b42bb685a5c8f2539af852a
Burhan Faquiri <burhan_faquiri@hotmail.com>
2023-10-07 16:29:44 -0400

    Project intialization

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptful",
+    description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <div className='main'>
+                    <div className='gradient' />
+
+                    <main className='app'>
+                        {children}
+                    </main>
+
+                </div>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout;
